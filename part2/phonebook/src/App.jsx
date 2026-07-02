fe46e06b3f58be14f7d4ee0371ea0455392348c7
Johnny Varila <jvarila@student.hive.fi>
2026-07-02 17:05:48 +0300

    feat: starting to work on phonebook exercise

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName,
+      id: String(persons.length + 1),
+    }
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <p key={person.name}>{person.name}</p>
+        )}
+      </ul>
+      <div>debug: {newName}</div>
+    </div>
+  )
+}
+
+export default App
