abf38e472db73b0141ed96269bd3575fd95af5f8
Johnny Varila <jvarila@student.hive.fi>
2026-07-10 17:23:37 +0300

    feat: added morgan middleware and handled more edge cases (missing data, wrong url)

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan((tokens, req,res) => {
+  let printable = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ]
+  if (tokens.method(req, res) === "POST")
+    printable = printable.concat(JSON.stringify(req.body))
+  return printable.join(' ')
+}))
 
 let persons = [
   {
@@ -60,22 +73,25 @@ const generateId = () => {
     return Math.floor(Math.random() * (maxFloor - minCeil + 1) + minCeil)
   }
 
-  return String(getRandomIntInclusive(1000000, 9999999))
+  return String(getRandomIntInclusive(100000000, 999999999))
 }
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
 
-  if (!body) {
-    return res.status(400).json({
-      error: 'body is missing'
-    })
-  }
+  if (!body)
+    return res.status(400).json({ error: 'body is missing' })
+  if (!body.name)
+    return res.status(400).json({ error: 'name is missing' })
+  if (!body.number)
+    return res.status(400).json({ error: 'number is missing' })
+  if (persons.filter(p => p.name === body.name).length)
+    return res.status(409).json({ error: 'name must be unique' })
 
   const p = {
+    id: generateId(),
     name: body.name,
     number: body.number,
-    id: generateId(),
   }
 
   persons = persons.concat(p)
@@ -89,6 +105,12 @@ app.delete('/api/persons/:id', (req, res) => {
 
   res.status(204).end()
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'uknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
