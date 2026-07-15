602ea9af54c33c56cd27cb7946a0a6ac6ce8f3aa
Johnny Varila <jvarila@student.hive.fi>
2026-07-15 15:11:00 +0300

    feat: added mongoose connection script to phonebook backend to connect to MongoDB Atlas

---
+++ part3/phonebook_backend/mongo.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.wmolm6n.mongodb.net/phonebookApp?
+retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+  Person.find({}).then(res => {
+    console.log('phonebook:')
+    res.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+  return
+}
+
+if (process.argv.length < 5) {
+  console.log('name or number is missing')
+  process.exit(1)
+}
+
+const name = process.argv[3]
+const number = process.argv[4]
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+})
+
+person.save().then(result => {
+  console.log(`added ${name} number ${number} to phonebook`)
+  mongoose.connection.close()
+})
