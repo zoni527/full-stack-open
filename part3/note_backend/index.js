9247f709c75c95ce80e873dc8d761676686553f2
Johnny Varila <jvarila@student.hive.fi>
2026-07-16 17:00:52 +0300

    fix(note_backend): changed all route handlers to use mongoose, reorganized middleware registration order, and added error handling middleware

--- part3/note_backend/index.js
+++ part3/note_backend/index.js
@@ -4,29 +4,37 @@ const Note = require('./models/note')
 
 const app = express()
 
-app.use(express.json())
-app.use(express.static('dist'))
+/* --------------------------------------------------------------- middleware */
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+const requestLogger = (req, res, next) => {
+  console.log('Method:', req.method)
+  console.log('Path:  ', req.path)
+  console.log('Body:  ', req.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError')
+    return res.status(400).send({ error: 'malformatted id' })
+
+  next(error)
+}
+
+/* -------------------------------------------------------------------------- */
+
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Note backend</h1>')
 })
 
 app.get('/api/notes', (req, res) => {
@@ -35,10 +43,15 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then(note => {
-    res.json(note)
-  })
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then(note => {
+      if (note)
+        res.json(note)
+      else
+        res.status(404).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (req, res) => {
@@ -57,31 +70,34 @@ app.post('/api/notes', (req, res) => {
   })
 })
 
-app.put('/api/notes/:id', (req, res) => {
-  const id = req.params.id
-  const existingNote = notes.find(note => note.id === id)
-  if (!existingNote)
-    return res.status(404).end()
+app.put('/api/notes/:id', (req, res, next) => {
+  const { content, important } = req.body
 
-  const body = req.body
-  if (!body || !body.content)
-    return res.status(400).json({ error: 'content missing' })
+  Note.findById(req.params.id)
+    .then(note => {
+      if (!note)
+        return res.status(404).end()
 
-  // console.log("request body:", body)
+      note.content = content
+      note.important = important
 
-  notes = notes.filter(n => n.id !== id).concat(body)
-
-  // console.log("notes:", notes)
-
-  res.json(body)
+      return note.save().then(updatedNote => {
+        res.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = req.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  res.status(204).end()
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
