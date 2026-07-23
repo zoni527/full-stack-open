4039967ed8e4de17a48038592d714241e6c636b4
Johnny Varila <jvarila@student.hive.fi>
2026-07-23 12:30:32 +0300

    style(note_backend): removed unnecessary space from message

--- part4/note_backend/app.js
+++ part4/note_backend/app.js
@@ -7,7 +7,7 @@ const notesRouter = require('./controllers/notes')
 
 const app = express()
 
-logger.info('connecting to ', config.MONGODB_URI)
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
   .connect(config.MONGODB_URI, { family: 4 })
