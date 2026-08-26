a33206323b41d5228ec411f65a6362265e9184f3
Johnny Varila <jvarila@student.hive.fi>
2026-08-26 15:41:36 +0300

    temp(blog_list_backend): start of supertest file

---
+++ part4/blog_list_backend/tests/blog_list_api.test.js
@@ -0,0 +1,8 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
