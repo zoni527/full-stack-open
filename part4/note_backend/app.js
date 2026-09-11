5ff098af646710118db2be0c521e939a86a095de
Johnny Varila <jvarila@student.hive.fi>
2026-09-11 16:32:31 +0300

    feat(note_backend): implemented JWT token issuing, also required for creating tokens

--- part4/note_backend/app.js
+++ part4/note_backend/app.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part4/note_backend/controllers/notes.js
+++ part4/note_backend/controllers/notes.js
@@ -1,6 +1,7 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note
@@ -17,10 +18,21 @@ notesRouter.get('/:id', async (request, response) => {
   }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id)
+    return response.status(401).json({ error: 'token invalid' })
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id)
 
   if (!user)
     return response.status(400).json({ error: 'userId missing or not valid' })
